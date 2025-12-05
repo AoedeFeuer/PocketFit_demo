3bde359fb56dfe7da58d5eaaac6f32ef740b474d
root <root@localhost.localdomain>
2025-12-05 13:12:33 +0100

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './reset.css'
+import { inject } from '@vercel/analytics';
+if (import.meta.env.PROD) inject();
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)

2541d9bc853245e9c6f0d56c21407248f7f69725
AoedeFeuer <root@localhost.localdomain>
2025-12-05 13:33:14 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,5 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './reset.css'
-import { inject } from '@vercel/analytics';
-if (import.meta.env.PROD) inject();
+import { inject } from '@vercel/analytics/next';
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
